c3c88347bbecad3b8d8654c61483c67722b84e79
salahbesbes <salah_besbes@ymail.com>
2020-12-22 19:11:19 +0100

    rotr

--- monty.h
+++ monty.h
@@ -110,6 +110,7 @@ void nop(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, __attribute__((unused)) unsigned int line_nb);
 
 
 #endif
